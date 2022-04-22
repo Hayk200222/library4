593428d0533a417702801b7b071eee05ba027b41
Hayk200222 <haykkyureghyan2@mail.ru>
2022-04-22 20:02:24 +0400

    Changes4

--- library3/src/main/java/com/library/library3/service/CSVService.java
+++ library3/src/main/java/com/library/library3/service/CSVService.java
@@ -1,9 +1,12 @@
 package com.library.library3.service;
 
-import com.library.library3.helper.CSVhelper;
+import com.library.library3.helper.CSVHelper;
+import com.library.library3.persistance.author.Author;
 import com.library.library3.persistance.book.Book;
+import com.library.library3.persistance.publisher.Publisher;
+import com.library.library3.repository.AuthorRepository;
 import com.library.library3.repository.BookRepository;
-import org.springframework.beans.factory.annotation.Autowired;
+import com.library.library3.repository.PublisherRepository;
 import org.springframework.stereotype.Service;
 import org.springframework.util.Assert;
 import org.springframework.web.multipart.MultipartFile;
@@ -15,16 +18,31 @@ import java.util.Optional;
 @Service
 public class CSVService {
 
-    @Autowired
-    BookRepository repository;
+    private final AuthorRepository authorRepository;
+    private final BookRepository bookRepository;
+    private final PublisherRepository publisherRepository;
 
-    public void save(MultipartFile file) {
-        Assert.notNull(file,"Passed file is null");
-        try {
-            List<Book> books = CSVhelper.csvToTuBooks(file.getInputStream());
-            repository.saveAll(books);
-        } catch (IOException e) {
-            throw new RuntimeException("fail to store csv data: " + e.getMessage());
+    public CSVService(AuthorRepository authorRepository, BookRepository bookRepository, PublisherRepository publisherRepository) {
+        this.authorRepository = authorRepository;
+        this.bookRepository = bookRepository;
+        this.publisherRepository = publisherRepository;
+    }
+
+    public void saveBooks(MultipartFile file) throws IOException {
+        Assert.notNull(file, "File must not be null");
+        List<Book> books = CSVHelper.parseCSV(file.getInputStream());
+        for (Book book : books) {
+
+            Optional<Author> optionalAuthor = authorRepository.findByFirstName(book.getAuthor().getFirstName());
+            Author author = authorRepository.save(optionalAuthor.orElseGet(() -> authorRepository.save(book.getAuthor())));
+            book.setAuthor(author);
+
+            Optional<Publisher> publisherOptional = publisherRepository.findByFirstName(book.getPublisher().getFirstName());
+            Publisher publisher = publisherRepository.save(publisherOptional.orElseGet(() -> publisherRepository.save(book.getPublisher())));
+            book.setPublisher(publisher);
+            bookRepository.save(book);
+
         }
     }
+
 }
